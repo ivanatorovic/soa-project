87100653ed577fea98fc3a012b71308266bcc19e
ivanatorovic <ivana.torovic2003@gmail.com>
2026-03-25 22:07:43 +0100

    Initial project structure with stakeholders and blog services

---
+++ stakeholders-service/src/test/java/com/soa/stakeholders_service/StakeholdersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soa.stakeholders_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StakeholdersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
